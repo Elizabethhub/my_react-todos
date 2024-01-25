c57e156cc2b793fcf62d82f5a641aaece8ca7628
Elizabethhub <bagiraliza21@gmail.com>
2024-01-26 00:47:09 +0200

    electrom+react setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./AppStyled";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <Container>
+      <TodoApp />
+    </Container>
+  );
+}
+
+export default App;
